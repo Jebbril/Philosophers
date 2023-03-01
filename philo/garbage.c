cc16ba33ee147a7cf6a9aa4b62673031ac0109c6
Omar Rakib <orakib@e2r6p4.1337.ma>
2023-03-01 14:37:42 +0100

    parsing

--- philo/garbage.c
+++ philo/garbage.c
@@ -1,38 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   garbage.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: orakib <orakib@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/25 14:21:30 by orakib            #+#    #+#             */
-/*   Updated: 2023/03/01 14:27:39 by orakib           ###   ########.fr       */
+/*   Created: 2023/02/28 13:29:43 by orakib            #+#    #+#             */
+/*   Updated: 2023/02/28 13:59:37 by orakib           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
+#include "philo.h"
 
-# define PHILO_H
-
-# include "sys/time.h"
-# include "pthread.h"
-# include "stdlib.h"
-# include "stdio.h"
-# include "string.h"
-# include "unistd.h"
-
-typedef struct s_var
+void	free_exit(t_var *v)
 {
-	int				*args;
-	pthread_t		*philos;
-	pthread_mutex_t	*forks;
-	int				s;
-	int				i;
-	int				j;
-}	t_var;
-
-int		*parsing(int ac, char **av);
-long	ft_atoi(const char *str);
-void	free_exit(t_var *v);
-
-#endif
+	if (v->args)
+		free(v->args);
+	if (v->philos)
+		free(v->philos);
+	if (v->forks)
+		free(v->forks);
+	exit(EXIT_FAILURE);
+}
--- philo/main.c
+++ philo/main.c
@@ -6,18 +6,60 @@
 /*   By: orakib <orakib@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 15:51:21 by orakib            #+#    #+#             */
-/*   Updated: 2023/02/25 18:00:16 by orakib           ###   ########.fr       */
+/*   Updated: 2023/03/01 14:34:44 by orakib           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	*func(void *arg)
+{
+	
+}
+
+void	init_mutexes(t_var *v)
+{
+	v->i = -1;
+	while (++v->i < v->args[0])
+	{
+		v->s = pthread_mutex_init(&v->forks[v->i], NULL);
+		if (v->s != 0)
+			free_exit(v);
+	}
+}
+
+void	create_threads(t_var *v)
+{
+	v->philos = malloc(sizeof(pthread_t) * v->args[0]);
+	if (!v->philos)
+		free_exit(v);
+	v->forks = malloc(sizeof(pthread_mutex_t) * v->args[0]);
+	if (!v->forks)
+		free_exit(v);
+	init_mutexes(v);
+	v->i = -1;
+	while (++v->i < v->args[0])
+	{
+		v->s = pthread_create(&v->philos[v->i], NULL, func, NULL);
+		if (v->s != 0)
+			free_exit(v);
+	}
+	v->j = -1;
+	while (++v->j < v->args[0])
+	{
+		v->s = pthread_join(v->philos[v->j], NULL);
+		if (v->s != 0)
+			free_exit(v);
+	}
+}
+
 int	main(int ac, char **av)
 {
-	int	*args;
+	t_var	v;
 
 	if (ac < 5 || ac > 6)
 		exit(EXIT_FAILURE);
-	args = parsing(ac, av);
-	
+	v.args = parsing(ac, av);
+	create_threads(&v);
+	free_exit(&v);
 }
